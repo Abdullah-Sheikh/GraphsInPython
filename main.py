308d561f41917f6e573c4da8b991f487ff54f5cd
Abdullah <abdullahsh123@hotmail.com>
2021-03-22 01:03:00 +0500

    Adjacent List Graph

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as lib1
 import networkx as lib2
 
-G = lib2.DiGraph()
-G
+G = lib2.Graph()
+G.add_edges_from([ ('A','B'),('A','C'),('B','C')])
+#G.add_weighted_edges_from([('A','B',4),('B','C',5)])
+pos = lib2.planar_layout(G)
+lib2.draw_networkx_nodes(G,pos,node_size=500)
+lib2.draw_networkx_edges(G,pos,edgelist=G.edges(),edge_color = 'black')
+lib2.draw_networkx_edge_labels(G,pos)
+lib2.draw_networkx_labels(G,pos)
+lib1.show()
